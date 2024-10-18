4a8439244874ed4d52b788d7fcf30be0bd60958e
steven-felim <stevenfelim04@gmail.com>
2024-10-18 15:30:13 +0700

    buat class Karyawan extends User

---
+++ src/Karyawan.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+
+public class Karyawan extends Staff{
+    private double salary;
+    private ArrayList<Presensi> presensiStaff = new ArrayList<Presensi>();
+
+    public Karyawan(String nama, String alamat, String ttl, String telepon, int nik, double salary, ArrayList<Presensi> presensiStaff) {
+        super(nama, alamat, ttl, telepon, nik);
+        this.salary = salary;
+        this.presensiStaff = presensiStaff;
+    }
+
+    public ArrayList<Presensi> getPresensiStaff() {
+        return presensiStaff;
+    }
+
+    public void setPresensiStaff(ArrayList<Presensi> presensiStaff) {
+        this.presensiStaff = presensiStaff;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
